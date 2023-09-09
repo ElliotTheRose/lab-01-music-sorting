00bff4e1fcfbdd5ada140f64988123cbd5cb87f1
ElliotTheRose <132631688+ElliotTheRose@users.noreply.github.com>
2023-09-09 17:33:17 -0400

    first try first try

--- music-sorting-starter-files/lab1.cpp
+++ music-sorting-starter-files/lab1.cpp
@@ -167,6 +167,9 @@ void MusicLibrary::get_options(int argc, char** argv) {
     // use getopt to find command line options
     struct option longOpts[] = {{ "print", required_argument, nullptr, 'p' },
                                 { "help", no_argument, nullptr, 'h' },
+                                { "name", no_argument, nullptr, 'n' },
+                                { "artist", no_argument, nullptr, 'a' },
+                                { "listens", no_argument, nullptr, 'l' },
                                 { nullptr, 0, nullptr, '\0' }};
     
     /*
@@ -177,7 +180,7 @@ void MusicLibrary::get_options(int argc, char** argv) {
                 char, options with no_argument do not (help).
 
     */
-    while ((option = getopt_long(argc, argv, "p:h", longOpts, &option_index)) != -1) {
+    while ((option = getopt_long(argc, argv, "p:hnal", longOpts, &option_index)) != -1) {
         switch (option) {
             case 'p':
                 num_print = std::atoi(optarg);
@@ -187,9 +190,17 @@ void MusicLibrary::get_options(int argc, char** argv) {
 
                 TODO: Add the remaining cases and decide what to do when they occur.
 
-            */               
-
-            case 'h':
+            */   
+           case 'n':
+                policy = 'n';
+                break;
+           case 'a':
+                policy = 'a';
+                break;
+           case 'l':  
+                policy = 'l';
+                break;           
+           case 'h':
                 std::cout << "This program reads a CSV file that contains song names,\n"
                           << "the artist who wrote them, and the number of plays each song\n"
                           <<  "has on Spotify.  It then outputs the number of songs specified\n"
@@ -310,6 +321,13 @@ void MusicLibrary::run() {
         TODO: Add the remaining sorting cases (sorting by artist and listens).
 
     */
+    if (policy == 'l') {
+        std::sort(music.begin(), music.end(), Song::ListensSort());
+    }
+    if (policy == 'a') {
+        std::sort(music.begin(), music.end(), Song::ArtistSort());
+    }
+    
 
     // Print out the first num_print songs with the
     // overloaded stream insertion operator.   
